eec51ac1098479561f551afba40492a4dc261bdc
Marcos Ronaldo Bello Saiquita <jesusflorezaviza@gmail.com>
2025-03-07 23:28:07 -0400

    Add files via upload

---
+++ Source/BomberMan_012025_/BomberMan_012025_.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BomberMan_012025_.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BomberMan_012025_, "BomberMan_012025_" );
+ 
